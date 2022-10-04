06fb2ee08a4e913419d1046101389a0895633001
Mikhaylov Anton <anion155@gmail.com>
2022-10-04 13:40:44 +0300

    fix: add import resolver

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -17,6 +17,11 @@ const config = {
   parserOptions: {
     project: "./tsconfig.json",
   },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+    },
+  },
   plugins: [
     "import",
     "react-hooks",
