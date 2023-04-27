f2e67d675f0c6f7275cc9d1d6d3091a1841a1a95
Mikhaylov Anton <anion155@gmail.com>
2023-04-27 03:37:44 +0300

    feat: add assert utility

--- packages/react-hooks/src/utils/__tests__/assert.spec.ts
+++ packages/react-hooks/src/utils/__tests__/assert.spec.ts
@@ -1,6 +1,6 @@
-import { expect, test } from "@jest/globals";
+import { expect, jest, test } from "@jest/globals";
 
-import { assert } from "../assert";
+import { assert, warning } from "../assert";
 
 test("assert", () => {
   const message = "DeveloperError: assert message";
@@ -10,4 +10,14 @@ test("assert", () => {
   expect(() => assert(null, message)).toThrow(message);
   expect(() => assert(0, message)).toThrow(message);
   expect(() => assert(false, message)).toThrow(message);
+});
+
+test("warning", () => {
+  const message = "DeveloperError: assert message";
+  const warn = jest.spyOn(console, "warn");
+
+  expect(warning(5, message)).toBeUndefined();
+  expect(warning(undefined, message)).toBeUndefined();
+  expect(warn).toHaveBeenCalledTimes(1);
+  expect(warn).toHaveBeenCalledWith(message);
 });
--- packages/react-hooks/src/utils/assert.ts
+++ packages/react-hooks/src/utils/assert.ts
@@ -1,3 +1,23 @@
-export function assert<T>(condition: T, message?: string): asserts condition {
-  if (!condition) throw new Error(message);
+const production = process.env.NODE_ENV === "production";
+
+export function assert(
+  condition: unknown,
+  message?: string
+): asserts condition {
+  if (condition) return;
+  throw new Error(message);
+}
+
+export function warning(condition: unknown, message?: string) {
+  if (condition) return;
+  if (production) return;
+
+  // eslint-disable-next-line no-console -- used in dev env only
+  console?.warn?.(message);
+
+  try {
+    throw new Error(message);
+  } catch (error) {
+    // Error thrown for developer convenience
+  }
 }
