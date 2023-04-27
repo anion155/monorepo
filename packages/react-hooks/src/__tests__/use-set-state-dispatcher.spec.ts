87ed4019d22c48998a1926c24d5569e787ee2265
Mikhaylov Anton <anion155@gmail.com>
2023-04-27 21:16:01 +0300

    chore: delete old tests

--- packages/react-hooks/src/__tests__/use-set-state-dispatcher.spec.ts
+++ packages/react-hooks/src/__tests__/use-set-state-dispatcher.spec.ts
@@ -14,26 +14,6 @@ describe("useSetStateDispatcher", () => {
     expect(hook.result.current).toStrictEqual(expect.any(Function));
   });
 
-  test("re-render with same deps", () => {
-    const hook = renderHook(() => useSetStateDispatcher(() => current, set));
-    const first = hook.result.current;
-    hook.rerender();
-
-    expect(hook.result.current).toBe(first);
-  });
-
-  // eslint-disable-next-line jest/no-disabled-tests -- deps are not usable in new version
-  test.skip("re-render with next deps", () => {
-    const hook = renderHook(
-      ({ deps }) => useSetStateDispatcher(() => current, set, deps),
-      { initialProps: { deps: [1] } }
-    );
-    const first = hook.result.current;
-    hook.rerender({ deps: [2] });
-
-    expect(hook.result.current).not.toBe(first);
-  });
-
   test("dispatch value", () => {
     const next = Symbol("test-next");
     const hook = renderHook(() => useSetStateDispatcher(get, set));
