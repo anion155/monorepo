f7c7ae78342d67820822da8d0d898a5feab03cd1
Mikhaylov Anton <anion155@gmail.com>
2023-04-27 04:17:14 +0300

    feat: update render effect hook

--- packages/react-hooks/src/use-render-effect.ts
+++ packages/react-hooks/src/use-render-effect.ts
@@ -1,30 +1,18 @@
 import type { DependencyList, EffectCallback } from "react";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
-type RenderEffectState = {
-  cleanup: ReturnType<EffectCallback>;
-  deps: DependencyList;
-};
+import { useRenderDispatcher } from "./utils/use-render-dispatcher";
 
 export function useRenderEffect(
   effect: EffectCallback,
   deps: DependencyList
 ): void {
-  const state = useRef<RenderEffectState>();
-  const prev = state.current;
-
-  useEffect(() => state.current?.cleanup, []);
-
-  if (prev && deps.length === prev.deps.length) {
-    let equal = true;
-    for (let index = 0; index < deps.length; index += 1) {
-      if (deps[index] !== prev.deps[index]) {
-        equal = false;
-      }
+  const destructor = useRenderDispatcher(
+    deps,
+    (cleanup: ReturnType<EffectCallback>) => {
+      cleanup?.();
+      return effect();
     }
-    if (equal) return;
-  }
-
-  prev?.cleanup?.();
-  state.current = { cleanup: effect(), deps };
+  );
+  useEffect(() => destructor, [destructor]);
 }
