9ad9de18e907a13465aa83b415c3d8880c8eac04
Mikhaylov Anton <anion155@gmail.com>
2023-04-28 13:59:23 +0300

    feat: move all eslint configs to module

--- packages/config/eslint.js
+++
@@ -1,9 +0,0 @@
-const tsConfig = require("@anion155/eslint-config/typescript");
-const jestConfig = require("@anion155/eslint-config/jest");
-
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@anion155"],
-  overrides: [jestConfig(), tsConfig()],
-};
