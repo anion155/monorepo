076b42ee0d2ec3c6e2e26c73dead69bdffca5291
Mikhaylov Anton <anion155@gmail.com>
2023-04-28 14:15:54 +0300

    feat: update bump script

--- packages/configs/bump-version.sh
+++ packages/configs/bump-version.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-pnpm version $1 && git add ./package.json && git commit -m "bump: react-hooks $1" && git tag "react-hooks-v$1"
+pnpm version $2 && git add ./package.json && git commit -m "bump: $1 $2" && git tag "$1-v$2"
