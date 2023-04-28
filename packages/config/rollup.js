f026c3ca8220bf3b9a2aa9b1abac8164d28dc8de
Mikhaylov Anton <anion155@gmail.com>
2023-04-28 14:07:53 +0300

    feat: update rollup config

--- packages/config/rollup.js
+++ packages/config/rollup.js
@@ -2,17 +2,16 @@ const commonjs = require("@rollup/plugin-commonjs").default;
 const nodeResolve = require("@rollup/plugin-node-resolve").default;
 const replace = require("@rollup/plugin-replace").default;
 const typescript = require("@rollup/plugin-typescript").default;
-const external = require("rollup-plugin-peer-deps-external");
-
 const fs = require("fs");
 const path = require("path");
+const external = require("rollup-plugin-peer-deps-external");
 
 /**
  * @param {boolean} production
  * @returns {import("rollup").Plugin[]}
  */
 function createPlugins(production) {
-  return[
+  return [
     external({ includeDependencies: true }),
     replace({
       preventAssignment: true,
@@ -28,13 +27,13 @@ function createPlugins(production) {
 exports.createPlugins = createPlugins;
 
 /**
- * @param {string} path
+ * @param {string} filePath
  */
-function fileI(path) {
+function fileI(filePath) {
   try {
-    const stats = fs.statSync(path);
+    const stats = fs.statSync(filePath);
     if (!stats.isFile()) return undefined;
-    return path;
+    return filePath;
   } catch {
     return undefined;
   }
@@ -49,36 +48,43 @@ function fileI(path) {
  *     input: string;
  *     output: string;
  *   } | {
-*     input: string;
-*     outputCjs: string;
-*     outputMjs: string;
-*   } | string;
+ *     input: string;
+ *     outputCjs: string;
+ *     outputMjs: string;
+ *   } | string;
  *   production?: boolean;
  *   outputOptions?: OutputOptions | OutputOptionsCreator;
  * }} config
+ * @param {import("rollup").MergedRollupOptions} [extension]
  * @returns {import("rollup").MergedRollupOptions}
  */
-function createLibraryInput(config) {
+function createLibraryInput(config, extension) {
   const prod = config.production ? ".prod" : "";
   const files = {
-    input: config.file.input
-      ?? fileI((config.file.input ?? "") + ".ts")
-      ?? fileI(path.join(config.file.input ?? "", "index.ts"))
-      ?? fileI(path.join("src", config.file ?? ""))
-      ?? fileI(path.join("src", (config.file ?? "") + ".ts"))
-      ?? fileI(path.join("src", config.file ?? "", "index.ts")),
-    outputCjs: config.file.outputCjs
-      ?? (config.file.output ? config.file.output + prod + ".js" : undefined)
-      ?? (path.join("dist", config.file + prod + ".js")),
-    outputMjs: config.file.outputMjs
-      ?? (config.file.output ? config.file.output + prod + ".mjs" : undefined)
-      ?? (path.join("dist", config.file + prod + ".mjs")),
+    input:
+      config.file.input ??
+      fileI(`${config.file.input ?? ""}.ts`) ??
+      fileI(path.join(config.file.input ?? "", "index.ts")) ??
+      fileI(path.join("src", config.file ?? "")) ??
+      fileI(path.join("src", `${config.file ?? ""}.ts`)) ??
+      fileI(path.join("src", config.file ?? "", "index.ts")),
+    outputCjs:
+      config.file.outputCjs ??
+      (config.file.output ? `${config.file.output + prod}.js` : undefined) ??
+      path.join("dist", `${config.file + prod}.js`),
+    outputMjs:
+      config.file.outputMjs ??
+      (config.file.output ? `${config.file.output + prod}.mjs` : undefined) ??
+      path.join("dist", `${config.file + prod}.mjs`),
   };
 
-  const createOutputOptions = typeof config.outputOptions === "function"
-    ? config.outputOptions : () => config.outputOptions;
+  const createOutputOptions =
+    typeof config.outputOptions === "function"
+      ? config.outputOptions
+      : () => config.outputOptions;
 
   return {
+    ...extension,
     input: files.input,
     output: [
       {
@@ -94,7 +100,10 @@ function createLibraryInput(config) {
         format: "es",
       },
     ],
-    plugins: createPlugins(config.production),
+    plugins: [
+      ...createPlugins(config.production),
+      ...(extension?.plugins ?? []),
+    ],
   };
 }
 exports.createLibraryInput = createLibraryInput;
--- packages/react-rxjs-hooks/rollup.config.js
+++ packages/react-rxjs-hooks/rollup.config.js
@@ -3,13 +3,13 @@ import { createLibraryInput } from "@monorepo/config/rollup";
 /** @type {import('rollup').MergedRollupOptions[]} */
 const config = [
   createLibraryInput({ file: "utils" }),
-  { ...createLibraryInput({ file: "index" }), external: ["./utils"] },
+  createLibraryInput({ file: "index" }, { external: ["./utils"] }),
 
   createLibraryInput({ file: "utils", production: true }),
-  {
-    ...createLibraryInput({ file: "index", production: true }),
-    external: ["./utils"],
-  },
+  createLibraryInput(
+    { file: "index", production: true },
+    { external: ["./utils"] }
+  ),
 ];
 
 export default config;
