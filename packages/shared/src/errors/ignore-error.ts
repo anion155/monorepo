18de8f55356a837d7508663f186c72203f601e25
Mikhaylov Anton <anion155@gmail.com>
2025-04-21 13:33:44 +0400

    feat: implement ignore error wrapper

--- packages/shared/src/errors/ignore-error.ts
+++ packages/shared/src/errors/ignore-error.ts
@@ -1,25 +1,40 @@
-import { curryHelper, identity, wrapFunctor } from "../functional";
+import { curryHelper, identity, wrapCallable } from "../functional";
 import { isError } from "../is";
 import { isPromise } from "../promise";
 
+/**
+ * Utility that waits for function result, and on specific error swaps it to result.
+ * Accepts both sync and async functions.
+ *
+ * @example
+ * const main = ignoreError(DeveloperError, -1)((argv) => {
+ *   if (Array.isArray(argv)) throw new DeveloperError('only array allowed');
+ *   return 0;
+ * });
+ * main([]); // 0
+ * main(5); // -1
+ */
 export function ignoreError<SubError extends Error, Result>(Class: { new (...params: never): SubError }, result: Result) {
   const isIgnoredError = isError.create(Class);
-  return curryHelper(
-    <Fn extends Method<never, never, unknown>>(fn: Fn): Fn =>
-      wrapFunctor(fn, function (this, ...params) {
-        try {
-          const result = fn.call(this, ...params);
-          if (isPromise(result)) {
-            return result.then(identity, (error) => {
-              if (isIgnoredError(error)) return result;
-              throw error;
-            });
+  return curryHelper(<Fn extends Method<never, never, unknown>>(fn: Fn) =>
+    wrapCallable<Fn, Fn>(
+      fn,
+      (call) =>
+        function () {
+          try {
+            const result = call();
+            if (isPromise(result)) {
+              return result.then(identity, (error) => {
+                if (isIgnoredError(error)) return result;
+                throw error;
+              });
+            }
+            return result;
+          } catch (error) {
+            if (isIgnoredError(error)) return result;
+            throw error;
           }
-          return result;
-        } catch (error) {
-          if (isIgnoredError(error)) return result;
-          throw error;
-        }
-      } as Fn),
+        } as Fn,
+    ),
   );
 }
