c3a0b8a971a377e7e3318f513fffc3b79ec3e2c7
Mikhailov Anton <anion155@gmail.com>
2025-10-10 03:45:51 +0400

    feat: implement action hooks

---
+++ packages/shared/src/react/use-action.spec.tsx
@@ -0,0 +1,129 @@
+import { describe, expect, it, jest } from "@jest/globals";
+import { Suspense } from "react";
+
+import { Action, InvalidActionState } from "../action";
+import { act, render, renderHook } from "./test-utils/render";
+import { useActionAwait, useActionCall, useActionResultState, useActionRunningState, useActionState } from "./use-action";
+
+describe("Action hooks", () => {
+  describe("useActionRunningState()", () => {
+    it("should update", async () => {
+      const deferred = Promise.withResolvers<string>();
+      const action = new Action(() => deferred.promise);
+      const hook = renderHook(useActionRunningState<[], string>, action);
+      expect(hook.result.current).toStrictEqual({ status: "idle" });
+
+      void action.run();
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ status: "pending", params: [], abort: expect.any(Function), promise: expect.any(Promise) });
+
+      deferred.resolve("test");
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ status: "idle" });
+    });
+  });
+
+  describe("useActionResultState()", () => {
+    it("should update", async () => {
+      const deferred = Promise.withResolvers<string>();
+      const action = new Action(() => deferred.promise);
+      const hook = renderHook(useActionResultState<[], string>, action);
+      expect(hook.result.current).toStrictEqual(undefined);
+
+      void action.run();
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual(undefined);
+
+      deferred.resolve("test");
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ status: "resolved", params: [], value: "test" });
+    });
+  });
+
+  describe("useActionState()", () => {
+    it("should update", async () => {
+      const deferred = Promise.withResolvers<string>();
+      const action = new Action(() => deferred.promise);
+      const hook = renderHook(useActionState<[], string>, action);
+      expect(hook.result.current).toStrictEqual({ status: "idle" });
+
+      void action.run();
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ status: "pending", params: [], promise: expect.any(Promise), abort: expect.any(Function) });
+
+      deferred.resolve("test");
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ status: "resolved", params: [], value: "test" });
+    });
+  });
+
+  describe("useActionAwait()", () => {
+    const renderSuspense = render.with(<Suspense fallback="loading" />);
+
+    it("should suspense value", async () => {
+      let deferred = Promise.withResolvers<string>();
+      const action = new Action((_param: number) => deferred.promise);
+      const spy = jest.fn();
+      const Test = ({ param = 1 }: { param?: number }) => {
+        spy(useActionAwait<[number], string>(action, param));
+        return "resolved";
+      };
+      const component = await renderSuspense.acted(<Test />);
+      expect(component.asFragment()).toHaveTextContent("loading");
+      expect(spy).not.toHaveBeenCalled();
+
+      deferred.resolve("test-1");
+      await act(() => Promise.resolve());
+      expect(component.asFragment()).toHaveTextContent("resolved");
+      expect(spy).toHaveBeenCalledWith("test-1");
+
+      spy.mockClear();
+      deferred = Promise.withResolvers();
+      component.rerender(<Test param={2} />);
+      expect(component.asFragment()).toHaveTextContent("loading");
+      expect(spy).not.toHaveBeenCalled();
+
+      deferred.resolve("test-2");
+      await act(() => Promise.resolve());
+      expect(component.asFragment()).toHaveTextContent("resolved");
+      expect(spy).toHaveBeenCalledWith("test-2");
+    });
+
+    it("should throw InvalidActionState", async () => {
+      const deferred = Promise.withResolvers<string>();
+      const action = new Action(() => deferred.promise);
+      const Test = () => {
+        useActionAwait<[], string>(action);
+        return "resolved";
+      };
+      const component = await renderSuspense.acted(<Test />);
+      expect(component.asFragment()).toHaveTextContent("loading");
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      delete (action as any)._private_running.promise;
+      expect(() => component.rerender(<Test />)).toStrictThrow(new InvalidActionState());
+    });
+  });
+
+  describe("useActionCall()", () => {
+    it("should suspense value", async () => {
+      const deferred = Promise.withResolvers<string>();
+      const spy = jest.fn((..._params: unknown[]) => deferred.promise);
+      const action = new Action(spy);
+      const hook = renderHook(useActionCall<unknown[], string>, action, 1, 2);
+
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({
+        action,
+        status: "pending",
+        params: [1, 2],
+        promise: expect.any(Promise),
+        abort: expect.any(Function),
+      });
+
+      deferred.resolve("test");
+      await act(() => Promise.resolve());
+      expect(hook.result.current).toStrictEqual({ action, status: "resolved", params: [1, 2], value: "test" });
+    });
+  });
+});
---
+++ packages/shared/src/react/use-action.tsx
@@ -0,0 +1,84 @@
+import type { ComponentProps, DependencyList, FunctionComponent } from "react";
+import { use, useEffect, useRef, useState } from "react";
+
+import type { ActionCallback, ActionRunningStatePending } from "../action";
+import { Action, InvalidActionState } from "../action";
+import { compare } from "../misc";
+import { useDeepMemo } from "./use-deep-memo";
+import { useRenderEffect } from "./use-render-effect";
+import { useStableCallback } from "./use-stable-callback";
+
+export const useActionRunningState = <Params extends unknown[], Result>(action: Action<Params, Result>) => {
+  const [state, setState] = useState(action.running);
+  useEffect(() => action.on("running", setState), [action]);
+  return state;
+};
+
+export const useActionResultState = <Params extends unknown[], Result>(action: Action<Params, Result>) => {
+  const [state, setState] = useState(action.result);
+  useEffect(() => action.on("result", setState), [action]);
+  return state;
+};
+
+export const useActionState = <Params extends unknown[], Result>(action: Action<Params, Result>) => {
+  const [state, setState] = useState(action.state);
+  useEffect(() => action.on("updated", setState), [action]);
+  return state;
+};
+
+export const useActionAwait = <Params extends unknown[], Result>(action: Action<Params, Result>, ...params: Params) => {
+  const state = useActionState(action);
+  if (state.status === "idle") return use(action.runCached(...params));
+  if (state.status === "pending") {
+    if (state.promise === undefined) throw new InvalidActionState();
+    return use(state.promise);
+  }
+  if (!compare(state.params, params, 1)) return use(action.runCached(...params));
+  if (state.status === "rejected") throw state.reason;
+  return state.value;
+};
+
+export const useActionCall = <Params extends unknown[], Result>(action: Action<Params, Result>, ...params: Params) => {
+  const memoizedParams = useDeepMemo(params);
+  useRenderEffect(() => {
+    void action.run(...memoizedParams);
+    return (action.running as ActionRunningStatePending<Params, Result>).abort;
+  }, [action, memoizedParams]);
+  return { ...useActionState(action), action };
+};
+
+export type MaybeAction<Params extends unknown[], Result> = Action<Params, Result> | ActionCallback<Params, Result>;
+export const useCreateAction = <Params extends unknown[], Result>(
+  maybeAction: MaybeAction<Params, Result>,
+  deps?: DependencyList,
+): Omit<Action<Params, Result>, "useAwait"> => {
+  const store = useRef<{ action: Action<Params, Result>; deps: DependencyList | undefined } | undefined>(undefined);
+  const stableActionCb = useStableCallback(maybeAction as ActionCallback<Params, Result>);
+  if (maybeAction instanceof Action) return maybeAction;
+  if (!store.current) {
+    store.current = { action: new Action(stableActionCb), deps };
+  } else if (!compare(store.current.deps, deps, 1)) {
+    store.current.action.cancel();
+    store.current = { action: new Action(stableActionCb), deps };
+  }
+  return store.current.action;
+};
+
+export const withCreateAction =
+  <Params extends unknown[], Result, Prop extends string>(maybeAction: MaybeAction<Params, Result>, propName: Prop) =>
+  <Component extends FunctionComponent<{ [P in Prop]: Action<Params, Result> }>>(
+    Component: Component,
+  ): Omit<Component, never> & {
+    (props: Omit<ComponentProps<Component>, Prop>): ReturnType<Component>;
+  } => {
+    const action = maybeAction instanceof Action ? maybeAction : new Action(maybeAction);
+    const Wrapped = (props: Omit<ComponentProps<Component>, Prop>) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return <Component {...(props as any)} {...{ [propName]: action }} />;
+    };
+    Object.setPrototypeOf(Wrapped, Component);
+    const name = `withCreateAction(${Component.displayName ?? Component.name})`;
+    Object.defineProperty(Wrapped, "name", { value: name, writable: false, enumerable: true, configurable: true });
+    Object.defineProperty(Wrapped, "displayName", { value: name, writable: false, enumerable: true, configurable: true });
+    return Wrapped as never;
+  };
