67ded9dcf9a5f2bc596c16a32bd5a03e1f6dc69e
Mikhailov Anton <anion155@gmail.com>
2025-12-08 05:36:35 +0400

    feat: implement generate.exports script

---
+++ packages/configs/scripts/generate.exports.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env jiti
+import { readdir, readFile, stat, writeFile } from "node:fs/promises";
+import path from "node:path";
+import { styleText } from "node:util";
+
+const SUPPORTED_EXTENSIONS = [".js", ".jsx", ".ts", ".tsx"];
+const UNSUPPORTED_EXTENSIONS = SUPPORTED_EXTENSIONS.map((ext) => `.spec${ext}`).concat(...SUPPORTED_EXTENSIONS.map((ext) => `.spec-d${ext}`));
+
+const add_dot = (filepath: string) => {
+  filepath = path.join(".", filepath);
+  if (filepath !== ".") filepath = "./" + filepath;
+  return filepath;
+};
+
+try {
+  const exports: Record<string, string> = {};
+  const traverseDir = async (dir: string, base: string) => {
+    for (const file of await readdir(dir)) {
+      const filepath = path.join(dir, file);
+      const basepath = path.join(base, file);
+      const fileStats = await stat(filepath);
+      if (fileStats.isDirectory()) {
+        await traverseDir(filepath, basepath);
+      } else if (SUPPORTED_EXTENSIONS.find((ext) => file === `index${ext}`)) {
+        exports[add_dot(base)] = add_dot(filepath);
+      } else if (SUPPORTED_EXTENSIONS.find((ext) => file.endsWith(ext)) && !UNSUPPORTED_EXTENSIONS.find((ext) => file.endsWith(ext))) {
+        exports[add_dot(basepath)] = add_dot(filepath);
+      }
+    }
+  };
+  await traverseDir("src", "");
+  const pkg: { exports?: Record<string, string> } = await readFile("./package.json", "utf-8").then(JSON.parse);
+  pkg.exports = exports;
+  await writeFile("./package_.json", JSON.stringify(pkg, undefined, 2) + "\n");
+
+  console.log(styleText("green", "Exports updated"));
+} catch (error) {
+  console.error(
+    styleText("red", "Exports generation failed:"),
+    typeof error === "object" && error !== null && "message" in error ? styleText("red", error.message as string) : (error as never),
+  );
+}
