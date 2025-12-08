2af9525dafafb37af0126f66dacac713f7ec993c
Mikhailov Anton <anion155@gmail.com>
2025-12-09 00:15:31 +0400

    fix(configs): remove extensions from exports

--- packages/configs/scripts/generate.exports.ts
+++ packages/configs/scripts/generate.exports.ts
@@ -11,6 +11,12 @@ const add_dot = (filepath: string) => {
   if (filepath !== ".") filepath = "./" + filepath;
   return filepath;
 };
+const module = (filepath: string) => {
+  filepath = add_dot(filepath);
+  const ext = SUPPORTED_EXTENSIONS.find((ext) => filepath.endsWith(ext));
+  if (ext) filepath = filepath.substring(0, filepath.length - ext.length);
+  return filepath;
+};
 
 try {
   const exports: Record<string, string> = {};
@@ -22,16 +28,16 @@ try {
       if (fileStats.isDirectory()) {
         await traverseDir(filepath, basepath);
       } else if (SUPPORTED_EXTENSIONS.find((ext) => file === `index${ext}`)) {
-        exports[add_dot(base)] = add_dot(filepath);
+        exports[module(base)] = add_dot(filepath);
       } else if (SUPPORTED_EXTENSIONS.find((ext) => file.endsWith(ext)) && !UNSUPPORTED_EXTENSIONS.find((ext) => file.endsWith(ext))) {
-        exports[add_dot(basepath)] = add_dot(filepath);
+        exports[module(basepath)] = add_dot(filepath);
       }
     }
   };
   await traverseDir("src", "");
   const pkg: { exports?: Record<string, string> } = await readFile("./package.json", "utf-8").then(JSON.parse);
   pkg.exports = exports;
-  await writeFile("./package_.json", JSON.stringify(pkg, undefined, 2) + "\n");
+  await writeFile("./package.json", JSON.stringify(pkg, undefined, 2) + "\n");
 
   console.log(styleText("green", "Exports updated"));
 } catch (error) {
